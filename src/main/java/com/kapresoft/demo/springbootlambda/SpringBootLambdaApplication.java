f38581598cd61a74b3b14d23aff29f2e192291ba
Tony Lagnada <tony@lagnada.com>
2019-04-27 18:33:00 -0700

    initial commit

---
+++ src/main/java/com/kapresoft/demo/springbootlambda/SpringBootLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.kapresoft.demo.springbootlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLambdaApplication.class, args);
+	}
+
+}
