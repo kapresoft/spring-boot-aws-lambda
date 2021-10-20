f21b13ab4729fe3141d65843be1b350ebaf8adae
Tony Lagnada <tony@lagnada.com>
2021-10-20 10:56:53 -0700

    update to spring boot 2.5.5

--- src/test/java/com/kapresoft/demo/springbootlambda/ApplicationIT.java
+++ src/test/java/com/kapresoft/demo/springbootlambda/ApplicationIT.java
@@ -1,15 +1,15 @@
 package com.kapresoft.demo.springbootlambda;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Slf4j
 @ActiveProfiles("it")
--- src/test/java/com/kapresoft/demo/springbootlambda/LambdaTest.java
+++ src/test/java/com/kapresoft/demo/springbootlambda/LambdaTest.java
@@ -1,7 +1,7 @@
 package com.kapresoft.demo.springbootlambda;
 
 import com.kapresoft.demo.springbootlambda.util.JsonUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
